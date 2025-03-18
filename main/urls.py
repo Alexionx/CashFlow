b3d3e91d42528205c20626723beb8453a6194c5f
exekx <holysheat24@gmail.com>
2025-03-18 20:50:03 +0200

    Add registration form

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
     path('profile/', views.profile, name='profile'),
     path('createPlan/', views.createPlan, name='createPlan'),
     path('currentBudget/', views.currentBudget, name='currentBudget'),
--- main/views.py
+++ main/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
+# Функція для реєстрації
 def register(request):
-    return render(request, 'registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Зберігає користувача в базу даних
+            login(request, user)  # Авторизує користувача після реєстрації
+            return redirect('home')  # Перенаправлення на сторінку профілю
+    else:
+        form = UserRegistrationForm()
 
+    return render(request, 'registration.html', {'form': form})
+
+# Функція для входу
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Перенаправлення на сторінку профілю після входу
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+# Функція для профілю
 def profile(request):
     return render(request, 'profile.html')
 
+# Інші функції
 def createPlan(request):
     return HttpResponse('<h4>Перевірка "Створення плану"</h4>')
 
