3c9de6c0e638c692a51d2a18989139f928bb44c8
exekx <holysheat24@gmail.com>
2025-04-23 02:58:22 +0300

    Protect pages from unauthorized access

--- cashflow/views.py
+++ cashflow/views.py
@@ -1,14 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-def home(request):
-    return HttpResponse('Hello, World!')
-
-
+@login_required
 def home(request):
     return render(request, 'home.html')
 
